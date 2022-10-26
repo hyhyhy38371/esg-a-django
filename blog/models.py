95ee50af013dc45812329ff898bc5a8443efd0ee
hyhyhy38371 <38371@naver.com>
2022-10-26 15:15:13 +0900

    blog 파일 수정

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,11 @@ from django.db import models
 
 # Post model
 class Post(models.Model):
-        title = models.CharField(max_length=30)
-        content = models.TextField()
+    title = models.CharField(max_length=30)
+    content = models.TextField()
 
-        created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-        def __Str__(self):
-            return self.title
+    def __str__(self):
+        return f"[{self.pk}] {self.title}"
