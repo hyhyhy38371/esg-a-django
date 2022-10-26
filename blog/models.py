04ae14cfee7bf30313efd52e77043ec9f895bbe0
hyhyhy38371 <38371@naver.com>
2022-10-26 14:19:22 +0900

    blog.post 모델/마이그레이션 생성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+# Post model
+class Post(models.Model):
+        title = models.CharField(max_length=30)
+        content = models.TextField()
+
+        created_at = models.DateTimeField()
+
+        def __Str__(self):
+            return self.title
